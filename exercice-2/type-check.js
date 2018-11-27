5fa06028e28f5818312fcfea444ba2e151feff69
Jérémie Noël <dev.jnoel@gmail.com>
2018-11-27 15:12:43 +0100

    v1 ci

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,10 +1,19 @@
 
-// type_check_v1('1', "number")
+// type_check_v1(undefined, "undefdined");
 
 function type_check_v1(val, type) {
     if(typeof val === type) {
         return true;
     } else {
-        return false;
+        switch(true) {
+            case Array.isArray(val) && type === "array":
+                return true;
+            case !val && type === "null":
+                return true;
+            case val && type === "undefined":
+                return true;
+            default:
+                return false;
+        }
     }
 }
